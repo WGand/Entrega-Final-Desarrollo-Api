9791a54a54bbec382bbaacbad7af9f3d74833244
dacarvajal.76 <dacarvajal.76@gmail.com>
2023-01-13 21:35:28 -0400

    Agregacion de los metodos faltantes

--- src/course/domain/CourseRepository.ts
+++ src/course/domain/CourseRepository.ts
@@ -1,6 +1,12 @@
 import { Result } from 'src/utils/Result';
 import { Course } from './Course';
+import { CourseIdVO } from './value_objects/CourseIdVO';
 
 export interface CourseRepository {
   createCourse(course: Course): Result<Course>;
+  getCourseById(id: CourseIdVO): Result<Course>;
+  deleteCourseById(id: CourseIdVO): Result<Course>
+  getAllCourses(): Result<Iterable<Course>>
 }
+
+
